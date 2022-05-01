31e5587177fe7084170016ae4502b6ec508a93dc
Jorge <jorgedrios86@gmail.com>
2022-05-01 12:10:49 -0300

    creacion  de funciones y efectos para el navbar

---
+++ script.js
@@ -0,0 +1,51 @@
+// declaracion de variables
+
+let efectoNavbar = document.querySelector(".navbar");
+let enlace = document.querySelectorAll(".enlaceNavbar");
+let logoNavbar = document.querySelector(".logoNavbar");
+
+let posicionScroll = 0;
+let ticking = false;
+
+// creacion de funciones
+
+// funcion afregar efecto en navbar
+function agregarEfecto(scroll_pos) {
+  efectoNavbar.classList.add("efecto");
+  logoNavbar.src = "../img/logo-brand.png"
+  enlace.forEach((element) => {
+    for (let index = 0; index < enlace.length; index++) {
+      element.style.color = "#000000";
+    }
+  });
+}
+
+// funcion quitar efecto en navbar
+function quitarEfecto(scroll_pos) {
+  efectoNavbar.classList.remove("efecto");
+  logoNavbar.src = "../img/logo-white.png"
+  enlace.forEach((element) => {
+    for (let index = 0; index < enlace.length; index++) {
+      element.style.color = "#ffffff";
+    }
+  });
+}
+
+// creacion de eventos
+
+// evento para el navbar
+window.addEventListener("scroll", function (e) {
+  posicionScroll = window.scrollY;
+  if (posicionScroll >= 5) {
+    window.requestAnimationFrame(function () {
+      agregarEfecto(posicionScroll);
+      ticking = false;
+    });
+  }
+  if (posicionScroll <= 3) {
+    window.requestAnimationFrame(function () {
+      quitarEfecto(posicionScroll);
+      ticking = false;
+    });
+  }
+});
